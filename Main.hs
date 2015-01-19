c650b77ff790acce7fab49060e330b3df95325b7
Spiehr <spiehr@gmx.de>
2015-01-19 22:04:10 +0100

    check if steric works

--- Main.hs
+++ Main.hs
@@ -10,15 +10,18 @@ import qualified Data.Map as M
 
 import qualified Tribes.Garyki as Gary
 import qualified Tribes.Mmpiki as Mmpi
+import qualified Tribes.Moving5 as Mv5
 
 
 helper :: ((Warrior, Intelligence) -> c) -> N.Nmap TribeName WarriorName c
 helper f =
     composeAll
-        ([("Holzfaeller", name) N.-<- f value |
-            (name, value) <- zip (M.keys Mmpi.provide) (M.elems Mmpi.provide)]
-      ++ [("Graeber", name) N.-<- f value |
-            (name, value) <- zip (M.keys Gary.provide) (M.elems Gary.provide)])
+        [("Holzfaeller", name) N.-<- f value |
+            (name, value) <- zip (M.keys Mv5.provide) (M.elems Mv5.provide)]
+      --   ([("Holzfaeller", name) N.-<- f value |
+      --       (name, value) <- zip (M.keys Mmpi.provide) (M.elems Mmpi.provide)]
+      -- ++ [("Graeber", name) N.-<- f value |
+      --       (name, value) <- zip (M.keys Gary.provide) (M.elems Gary.provide)])
        $ N.empty
 
 
