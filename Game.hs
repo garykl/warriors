3d1ea879e1f73306781acf9dde2fde5293c2ce08
Spiehr <spiehr@gmx.de>
2015-01-19 22:28:51 +0100

    add random functionality and use it for movement so far

--- Game.hs
+++ Game.hs
@@ -6,6 +6,38 @@ import qualified Observe as O
 import qualified NestedMap as N
 import Geometry
 
+import System.Random (randomIO)
+import System.IO.Unsafe (unsafePerformIO)
+
+
+-- | produce a random number from 0 to n
+randomTo :: Int -> Int
+randomTo n = unsafePerformIO randomIO `mod` n
+
+
+-- | random number from n to m
+randomFromTo :: Int -> Int -> Int
+randomFromTo n m = n + randomTo m
+
+
+-- | random float between low and high
+-- this function needs argument, for returning random number. An implementation
+-- without argument was memoizing the first used random number.
+random :: Float -> Float -> Float
+random low high = low + (high - low) * fromIntegral (randomTo 100) / 100
+
+
+-- | blur a data type
+class Randomizable a where
+    -- | the first argument is the blurring amount, the second one is to be
+    -- blurred.
+    randomize :: a -> a -> a
+
+instance Randomizable Position where
+    randomize (Pos rx ry) (Pos x y) =
+        Pos (random (x - rx) (x + rx))
+            (random (y - ry) (y + ry))
+
 
 -- | for knowing what the @Effect@ of an @Action@ is, we need the @Field@,
 -- the @Warrior@ tht performs the @Action@ and the @Action@ itself. Multiple
@@ -50,7 +82,9 @@ actionToEffects field wid action =
 
                 -- compute what future could bring
                 let direction = normalize $ pos .-. position agent
-                    futurePosition = position agent .+ (velocity soul |*| direction)
+                    futurePosition =
+                        randomize (Pos 1 1)
+                                $ position agent .+ (velocity soul |*| direction)
                     futureAgent = agent { position = futurePosition }
                     effect ag = ag {
                         position = futurePosition,
