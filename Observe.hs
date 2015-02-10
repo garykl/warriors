6e88d8d65be352adf9dada86ef0ff409ae53fe88
Spiehr <spiehr@gmx.de>
2015-02-10 23:03:11 +0100

    FeatureExtraction is now Applicative, not sure if laws hold

--- Observe.hs
+++ Observe.hs
@@ -6,14 +6,28 @@ import qualified Logic as L
 import Geometry
 import qualified Data.Map as M
 import Data.Functor ((<$>))
+import Control.Applicative
 
 
 data FeatureExtraction a = FE (L.Environment -> a)
 
 
 instance Functor FeatureExtraction where
-    -- fmap :: (a -> b) -> FeatureExtraction a -> FeatureExtraction b
     fmap f (FE fef) = FE (f . fef)
+
+
+instance Applicative FeatureExtraction where
+    pure a = FE (const a)
+    (FE f) <*> (FE a) = FE (\env -> f env (a env))
+
+
+-- I guess this is not useful
+-- instance Monad FeatureExtraction where
+--     return a = FE (const a)
+--     (FE a) >>= f = extract $ f . a
+--       where extract :: (L.Environment -> FeatureExtraction a)
+--                     -> FeatureExtraction a
+--             extract fe = fe (L.Env M.empty [])
 
 
 enemies :: FeatureExtraction [W.Warrior]
@@ -28,7 +42,7 @@ wherestCharacter :: ([Vector] -> Vector)
                  -> FeatureExtraction [W.Warrior]
                  -> W.Warrior -> FeatureExtraction Vector
 wherestCharacter g few warrior =
-    g <$> map (W.liftAgents W.agentToAgentVector warrior) <$> few
+    g . map (W.liftAgents W.agentToAgentVector warrior) <$> few
 
 
 -- | return the relative position of the nearest enemy
